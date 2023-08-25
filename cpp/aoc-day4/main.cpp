cef6ec76636f324632f66ae000a84631932ad264
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-08-24 23:25:41 -0300

    feat: start implement fully contains function on day4 using c++

--- cpp/aoc-day4/main.cpp
+++ cpp/aoc-day4/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -6,12 +7,18 @@
 using namespace std;
 
 class Pair {
-  public:
-    string firstSection;
-    string secondSection;
+public:
+  int start;
+  int end;
 };
 
-void readFileData(string & fileName, vector<string>& pairsString) {
+class Section {
+public:
+  Pair first;
+  Pair second;
+};
+
+void readFileData(string &fileName, vector<string> &pairsString) {
   ifstream file{};
 
   file.open(fileName);
@@ -29,32 +36,163 @@ void readFileData(string & fileName, vector<string>& pairsString) {
   }
 }
 
-vector<Pair> createPairs(vector<string>& pairsString) {
-  vector<Pair> pairs;
+vector<Section> readPairs(vector<string> &pairsString) {
+  vector<Section> sections;
 
-  for (auto const &section : pairsString) {
+  for (auto const &sectionString : pairsString) {
     Pair pair;
+    Section section;
+    string firstSection;
+    string secondSection;
     bool firstSectionFilled = false;
-    
-    for (auto const &character : section) {
+
+    for (auto const &character : sectionString) {
       if (character == ',') {
         firstSectionFilled = true;
         continue;
       }
 
       if (!firstSectionFilled) {
-        pair.firstSection.push_back(character);
+        firstSection.push_back(character);
 
         continue;
       }
 
-      pair.secondSection.push_back(character);
+      secondSection.push_back(character);
     }
 
-    pairs.push_back(pair);
+    string sectionValue;
+
+    for (int i = 0; i <= firstSection.size(); i++) {
+      char character = firstSection[i];
+
+      if (character == '-') {
+        int convertedSectionValue = atoi(sectionValue.c_str());
+        pair.start = convertedSectionValue;
+        sectionValue = "";
+        continue;
+      }
+
+      sectionValue.push_back(character);
+
+      if (i == firstSection.size() - 1) {
+        int convertedSectionValue = atoi(sectionValue.c_str());
+        pair.end = convertedSectionValue;
+        sectionValue = "";
+        continue;
+      }
+    }
+
+    section.first = pair;
+    sectionValue = "";
+
+    for (int i = 0; i <= secondSection.size(); i++) {
+      char character = secondSection[i];
+
+      if (character == '-') {
+        int convertedSectionValue = atoi(sectionValue.c_str());
+        pair.start = convertedSectionValue;
+        sectionValue = "";
+        continue;
+      }
+
+      sectionValue.push_back(character);
+
+      if (i == firstSection.size() - 1) {
+        int convertedSectionValue = atoi(sectionValue.c_str());
+        pair.end = convertedSectionValue;
+        sectionValue = "";
+        continue;
+      }
+    }
+
+    section.second = pair;
+
+    sections.push_back(section);
   }
 
-  return pairs;
+  return sections;
+}
+
+int fullyContainsCount(const vector<Section> &sections) {
+  int fullyContains = 0;
+
+  for (auto const &section : sections) {
+    Pair firstPair = section.first;
+    Pair secondPair = section.second;
+    vector<int> firstPairValues;
+    vector<int> secondPairValues;
+
+    for (int i = firstPair.start; i <= firstPair.end; i++) {
+      firstPairValues.push_back(i);
+    }
+
+    for (int i = secondPair.start; i <= secondPair.end; i++) {
+      secondPairValues.push_back(i);
+    }
+
+    if (firstPair.start == firstPair.end) {
+      firstPairValues.push_back(firstPair.start);
+    }
+
+    if (secondPair.start == secondPair.end) {
+      firstPairValues.push_back(secondPair.start);
+    }
+
+    bool contains = true;
+
+    if (firstPair.start < secondPair.start && firstPair.end >= secondPair.end) {
+      for (int i = 0; i < secondPairValues.size(); i++) {
+        int secondValue = secondPairValues[i];
+        int firstValue;
+        int firstValueAccumulator = 0;
+        while(firstValue != secondPairValues.back()) {
+          firstValue = firstPairValues[firstValueAccumulator]; 
+          if (firstValue < secondPair.start) {
+            firstValueAccumulator = firstValueAccumulator + 1;
+
+            continue;
+          }
+
+          if (firstValue != secondValue) {
+            contains = false;
+            i = secondPairValues.size();
+
+            break;
+          }
+        }
+      }
+    }
+
+   if (secondPair.start < firstPair.start && secondPair.end >= firstPair.end) {
+      for (int i = 0; i < firstPairValues.size(); i++) {
+        int secondValue = firstPairValues[i];
+        int firstValue;
+        int firstValueAccumulator = 0;
+        while(firstValue != firstPairValues.back()) {
+          firstValue = secondPairValues[firstValueAccumulator]; 
+          if (firstValue < secondPair.start) {
+            firstValueAccumulator = firstValueAccumulator + 1;
+
+            continue;
+          }
+
+          if (firstValue != secondValue) {
+            contains = false;
+            i = secondPairValues.size();
+
+            break;
+          }
+        }
+      }
+    }
+
+    if (contains) {
+      fullyContains = fullyContains++;
+    }
+  }
+
+  return fullyContains;
 }
 
 int main() {
@@ -63,9 +201,11 @@ int main() {
 
   readFileData(fileName, pairsString);
 
-  vector<Pair> pairs = createPairs(pairsString);
+  vector<Section> sections = readPairs(pairsString);
 
-  std::cout << pairs[0].firstSection;
+  int fullyContains = fullyContainsCount(sections);
+
+  cout << fullyContains;
 
   return 0;
 }
